89481806d891c1e1d2b7a7e9226847bbd4b1cdaf
xplorekamrul <mdkamruzzaman099826@gmail.com>
2025-11-05 11:59:11 +0600

    tiny update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 // /app/layout.tsx
+import Sidebar from "@/components/layout/Sidebar";
+import AppProviders from "@/components/providers/AppProviders";
 import type { Metadata } from "next";
 import "./globals.css";
-import AppProviders from "@/components/providers/AppProviders";
-import Navbar from "@/components/layout/Navbar";
 
-export const metadata: Metadata = { title: "YourApp" };
+export const metadata: Metadata = { title: "Hr_Plus" };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full">
       <body className="min-h-screen bg-light dark:bg-background text-foreground">
         <AppProviders>
-          <Navbar />
+          <Sidebar />
           <main className="mx-auto w-full max-w-6xl px-4 py-6">{children}</main>
         </AppProviders>
       </body>
